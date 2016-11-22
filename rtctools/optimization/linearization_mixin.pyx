ad97124e98618a602c56f0a8dd6aa67638aaca24
Jorn Baayen <jorn.baayen@deltares.nl>
2016-11-22 14:04:44 +0100

    Added mixin to help with the bookkeeping of linearization parameters.

---
+++ rtctools/optimization/linearization_mixin.pyx
@@ -0,0 +1,33 @@
+# cython: embedsignature=True
+
+from rtctools.optimization.optimization_problem import OptimizationProblem
+from rtctools.optimization.timeseries import Timeseries
+import itertools
+import logging
+
+logger = logging.getLogger("rtctools")
+
+
+class LinearizationMixin(OptimizationProblem):
+    """
+    Adds linearized equation parameter bookkeeping to your optimization aproblem.  If your model contains 
+    linearized equations, this mixin will set the parameters of these equations based on the t0 value of an associated
+    timeseries.
+
+    The mapping between linearization parameters and time series is provided in the ``linearization_parameters`` method.
+    """
+
+    def parameters(self, ensemble_member):
+        parameters = super(LinearizationMixin, self).parameters(ensemble_member)
+
+        for parameter, timeseries_id in self.linearization_parameters().iteritems():
+            parameters[parameter] = self.timeseries_at(timeseries_id, self.initial_time, ensemble_member)
+
+        return parameters
+
+    def linearization_parameters(self):
+        """
+        :returns: A dictionary of parameter names mapping to time series identifiers.
+        """
+
+        return {}
