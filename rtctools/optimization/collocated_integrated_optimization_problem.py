7c0e9b16ae2b081780dc3da47538f2f0911998de
Jorn Baayen <jorn.baayen@deltares.nl>
2017-05-15 11:46:32 +0200

    Remove unused casadi imports

--- rtctools/optimization/collocated_integrated_optimization_problem.py
+++ rtctools/optimization/collocated_integrated_optimization_problem.py
@@ -1,6 +1,6 @@
 # cython: embedsignature=True
 
-from casadi import MX, Function, rootfinder, nlpIn, nlpOut, vertcat, horzcat, jacobian, vec, substitute, sum1, sum2, interp1d, transpose, repmat, depends_on, reshape, mul
+from casadi import MX, Function, rootfinder, vertcat, horzcat, jacobian, vec, substitute, sum1, sum2, interp1d, transpose, repmat, depends_on, reshape, mul
 from abc import ABCMeta, abstractmethod
 import numpy as np
 import itertools
