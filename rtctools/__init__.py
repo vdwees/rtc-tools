412764f9aa6b1b7047f67d5bbef4dd9acdfffbd7
Jorn Baayen <jorn.baayen@deltares.nl>
2017-08-03 18:09:39 +0200

    Adjust statup message for new version strings.

--- rtctools/__init__.py
+++ rtctools/__init__.py
@@ -6,12 +6,12 @@ del get_versions
 # Print header
 print(
 """
-******************************************************************************
-             This program contains RTC-Tools {}, a toolbox for
-              control and optimization of environmental systems.                           
-    RTC-Tools is open source software under the GNU General Public License.
-   For more information visit https://www.deltares.nl/en/software/rtc-tools/
-******************************************************************************
+*****************************************************************************
+  This program contains RTC-Tools {}, 
+  a toolbox for control and optimization of water systems.                        
+  RTC-Tools is open source software under the GNU General Public License.
+  For more information visit https://www.deltares.nl/en/software/rtc-tools/
+*****************************************************************************
 """.format(__version__))
 
 # Import dependencies
