72e691b8e638a3556819a99f44f3f00cc7279c64
Jorn Baayen <jorn.baayen@deltares.nl>
2017-08-03 18:25:45 +0200

    Since pymola and casadi are available using pip, we do not need the mock module any longer.

--- rtctools/__init__.py
+++ rtctools/__init__.py
@@ -13,17 +13,3 @@ print(
   For more information visit https://www.deltares.nl/en/software/rtc-tools/
 *****************************************************************************
 """.format(__version__))
-
-# Import dependencies
-try:
-	import casadi
-except ImportError:
-	# Don't require casadi for readthedocs.org
-	from mock import MagicMock
-	import warnings
-	import sys
-
-	warnings.warn("CasADi not found.  Please ensure that casadi can be imported before running RTC-Tools.")
-
-	MOCK_MODULES = ['casadi', 'pymola', 'pylab', 'scipy', 'scipy.interpolate']
-	sys.modules.update((mod_name, MagicMock()) for mod_name in MOCK_MODULES)
